366f9189742d0d51d2d9b586695d31284c27f98d
Frank <f.neumann.berlin@googlemail.com>
2020-07-10 11:05:54 +0200

    mit Package

--- src/de/wbs/gui/FXDemo.java
+++ src/de/wbs/gui/FXDemo.java
@@ -1,21 +1,32 @@
 package de.wbs.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
-
 public class FXDemo extends Application {
+
+    public FXDemo() {
+    }
+
+    @FXML
+    private void initialize() {
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
+    @FXML
+    Label label;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -24,8 +35,16 @@ public class FXDemo extends Application {
         primaryStage.setTitle("Test");
         primaryStage.setScene(scene);
         primaryStage.sizeToScene();
+
+
         primaryStage.show();
     }
 
 
+    @FXML
+    public void btn1OnClick(MouseEvent mouseEvent) {
+        System.out.println("Es wurde der Knopf gedrückt");
+        label.setText("Button wurde geklickt");
+
+    }
 }
