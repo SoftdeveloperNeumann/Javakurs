3cc0d0dec11a457606ca9480680b0e5028aaaddd
Frank <f.neumann.berlin@googlemail.com>
2020-07-10 07:47:32 +0200

    mit Package

--- src/de/wbs/gui/FXDemo.java
+++ src/de/wbs/gui/FXDemo.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,7 +22,10 @@ public class FXDemo extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("Maingui.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setTitle("Test");
+        primaryStage.setScene(scene);
         primaryStage.sizeToScene();
         primaryStage.show();
     }
+
+
 }
