5c46104e0b2101fb2184e055137b47f13a9dbabb
Frank <f.neumann.berlin@googlemail.com>
2020-07-16 09:05:59 +0200

    mit Package

---
+++ src/de/wbs/kotlindemo/Person.java
@@ -0,0 +1,10 @@
+package de.wbs.kotlindemo;
+
+public class Person {
+    private String lastname;
+    private String firstname;
+    private String street;
+    private String number;
+    private String zip;
+    private String city;
+}
